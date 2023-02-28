eb52ba222cc556ecf3d06f2ef69566587da4e526
Spatraa <88728451+Spatraa@users.noreply.github.com>
2023-02-28 20:44:35 +0530

    Create Question1.js

---
+++ Question1.js
@@ -0,0 +1,12 @@
+function findElementsWithSumZero(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === 0) {
+                return arr[i]+" "+arr[j];
+            }
+        }
+    }
+    return "No Elements found";
+}
+console.log(findElementsWithSumZero([-4, 1, 3, -2, -1]));  // prints [-1, 1]
+console.log(findElementsWithSumZero([-1, 2, 3, 5]));  // prints "No Elements   
